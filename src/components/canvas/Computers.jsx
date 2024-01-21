928be49e334ceec1457f89e723e141e12efa2f7d
Lam Ching Rou <crlam.2020@scis.smu.edu.sg>
2024-01-21 22:42:55 +0800

    add mobile responsiveness for 3D component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,13 +2,18 @@ import { React, Suspense, useEffect, useState} from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../loader'
+import { useMediaQuery } from 'react-responsive';
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 640px)' });
+  const scale = isSmallScreen ? 0.4 : 0.7;
+  const position = isSmallScreen ? [0, -0.5, -0.5] : [0, -5.5, -1];
+
   return (
     <mesh>
       <hemisphereLight intensity={1} groundColor="black"/>
-      <primitive object={computer.scene} scale={0.7} position={[0, -6, -1]}/>
+      <primitive object={computer.scene} scale={scale} position={position}/>
     </mesh>
   )
 }
