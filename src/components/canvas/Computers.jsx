a5344222d9399f59c0779e760cb2e0d407e73511
Lam Ching Rou <crlam.2020@scis.smu.edu.sg>
2024-01-21 22:21:42 +0800

    fix reload

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -17,7 +17,7 @@ const ComputersCanvas = () => {
   return (
     <Canvas frameloop="demand" shadows camera={{position: [20, 3, 5], fov: 25}} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader/>}>
-        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} /> {/* only rotate horizontally */}
         <Computers/>
       </Suspense>
       <Preload all/>
