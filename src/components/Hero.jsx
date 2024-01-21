28b721b2c9c1c01bd8a716a323e5c4a171784364
Lam Ching Rou <crlam.2020@scis.smu.edu.sg>
2024-01-21 21:01:07 +0800

    add background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { styles } from '../styles'
+import { motion } from 'framer-motion'
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-fulll h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { navLinks } from '../constants';
 import {styles} from '../styles';
 import { menu, close } from "../assets";
@@ -9,12 +9,11 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <BrowserRouter>
       <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
         <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-          <Link to="/" className="flex items-center" onClick={()=>{SecurityPolicyViolationEvent(""); window.scrollTo(0, 0)}}>
-            <p className="text-white text-[18px] font-bold cursor-pointer">Lam Ching Rou &nbsp; <span className="sm:block hidden">| Aspiring software engineer</span></p>
-          </Link>
+          {/* <Link to="/" className="flex items-center" onClick={()=>{SecurityPolicyViolationEvent(""); window.scrollTo(0, 0)}}>
+            <p className="text-white text-[18px] font-bold cursor-pointer">Lam Ching Rou &nbsp; <span className="sm:block hidden">Aspiring Software Engineer</span></p>
+          </Link> */}
           <ul className="list-none hidden sm:flex floex-row gap-10">
             {navLinks.map((link) => (
               <li key={link.id} className={`${active === link.title ? "text-white": "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
@@ -22,25 +21,23 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+        </div>
 
           {/* on mobile */}
           <div className="sm:hidden flex flex-1 justify-end items-center">
-              <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle(!toggle)}/>
-              
-              <div className={`${!toggle ? 'hidden':'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
-                <ul className="list-none flex justiy-end items-start flex-col gap-4">
-                  {navLinks.map((link) => (
-                    <li key={link.id} className={`${active === link.title ? "text-white": "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`} onClick={() => {setActive(link.title); setToggle(!toggle)}}>
-                      <a href={`#${link.id}`}>{link.title}</a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              
+            <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle(!toggle)}/>
+            
+            <div className={`${!toggle ? 'hidden':'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+              <ul className="list-none flex justiy-end items-start flex-col gap-4">
+                {navLinks.map((link) => (
+                  <li key={link.id} className={`${active === link.title ? "text-white": "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`} onClick={() => {setActive(link.title); setToggle(!toggle)}}>
+                    <a href={`#${link.id}`}>{link.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div> 
           </div>
-        </div>
       </nav>
-    </BrowserRouter>
   )
 }
 
